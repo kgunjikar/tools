2b31bc1e79a0a172ef36749ae6ac07cd7e6aa3e5
kgunjikar <gunjikar@gmail.com>
2020-05-13 13:43:10 -0700

    Create decoder2.go

---
+++ decoder2.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	rbacv1 "k8s.io/api/rbac/v1"
+	"k8s.io/client-go/kubernetes/scheme"
+)
+
+func main() {
+	filecontent, err := ioutil.ReadFile("/Users/kshitijgunjikar/Downloads/cert-manager.yaml")
+	if err != nil {
+		fmt.Printf("%s", err.Error())
+		return
+	}
+	decode := scheme.Codecs.UniversalDeserializer().Decode
+	obj, _, _ := decode([]byte(filecontent), nil, nil)
+
+	fmt.Printf("%++v\n\n", obj.GetObjectKind())
+	fmt.Printf("%++v\n\n", obj)
+
+	cbl := obj.(*rbacv1.ClusterRoleBindingList) // This fails
+	fmt.Printf("%++v\n", cbl)
+
+	switch o := obj.(type) {
+	case *rbacv1.ClusterRoleBindingList:
+		fmt.Println("correct found") // Never happens
+	default:
+		fmt.Println("default case")
+		_ = o
+	}
+}
